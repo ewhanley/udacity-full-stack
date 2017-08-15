9891523ad2223d5479786b00645c995159d79394
Eric Hanleyy <ewhanley@gmail.com>
2017-08-14 21:23:51 -0700

    unsaved changes

--- movie-website/media.py
+++ movie-website/media.py
@@ -1,14 +1,23 @@
 import webbrowser
+import requests
+import json
 
 class Movie():
-    """ This class provides a way to store movie related information."""
-    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
+    """ This class provides a way to store movie related information."""    
+    def __init__(self, imdb_id):
+        self.title = 
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    def get_movie_info(self, imdb_id):
+        url = 'https://theimdbapi.org/api/movie?movie_id='+imdb_id
+        response = requests.get(url)
+        response_dict = json.loads(response.text)
+        self.title = response_dict['title']
+        self.storyline = response_dict['description']
+        self.poster_image_url = response_dict['poster']['larger']
+        self.trailer_video_url = 
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
--- movie-website/movie_api_test.py
+++ movie-website/movie_api_test.py
@@ -6,6 +6,6 @@ url = 'https://theimdbapi.org/api/movie?movie_id='+id
 
 response = requests.get(url)
 resp_dict = json.loads(response.text)
-print(resp_dict['poster']['thumb'])
+print(resp_dict['poster']['large'])
 
 
