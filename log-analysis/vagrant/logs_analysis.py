c8b7af3f88711eae23e95941a593ef51cf3697d4
Eric Hanley <ewhanley@gmail.com>
2017-09-12 22:23:02 -0700

    Renamed variables

--- log-analysis/vagrant/logs_analysis.py
+++ log-analysis/vagrant/logs_analysis.py
@@ -3,12 +3,11 @@
 import psycopg2
 from tabulate import tabulate
 
-DBNAME = "news"
-
-conn = psycopg2.connect(dbname=DBNAME)
+conn = psycopg2.connect(dbname='news')
 cursor = conn.cursor()
 
-sql_query1 = """
+question1 = "What are the three most popular articles of all time?"
+query1 = """
         SELECT a.title, count(a.title) as views
         FROM articles a, log l
         WHERE a.slug = SUBSTRING(l.path, '[^/]*$')
@@ -16,7 +15,13 @@ sql_query1 = """
         GROUP BY a.title
         ORDER BY views DESC;"""
 
-sql_query2 = """
+cursor.execute(query1)
+query1_out_raw = cursor.fetchall()
+query1_out_tbl = tabulate(query1_out_raw, headers=['Title', 'Views'],
+                          tablefmt="orgtbl")
+
+question2 = "Who are the most popular article authors of all time?"
+query2 = """
         SELECT au.name, COUNT(l.path) AS views
         FROM authors au, articles ar, log l
         WHERE ar.slug = SUBSTRING(l.path, '[^/]*$')
@@ -25,37 +30,44 @@ sql_query2 = """
         GROUP BY au.name
         ORDER BY views DESC;"""
 
-sql_query3 = """
+cursor.execute(query2)
+query2_out_raw = cursor.fetchall()
+query2_out_tbl = tabulate(query2_out_raw, headers=['Author', 'Views'],
+                          tablefmt="orgtbl")
+
+question3 = "On which days did more than 1% of requests lead to errors?"
+query3 = """
         SELECT TO_CHAR(date, 'Month dd, YYYY') AS date,
             (ROUND(fraction * 100, 2)::text || '%') AS percent_errors
-        FROM(
-            SELECT DISTINCT DATE_TRUNC('day', time) AS date, status,
-                (COUNT(status) OVER dsw::decimal) / (COUNT(status) OVER dw) AS FRACTION
-            FROM log
-            WINDOW dsw AS (PARTITION BY DATE_TRUNC('day', time), status),
-                dw AS (PARTITION BY DATE_TRUNC('day', time))
-            ) as error_counts
+        FROM
+        (
+        SELECT DISTINCT DATE_TRUNC('day', time) AS date, status,
+        (COUNT(status) OVER dsw::decimal) / (COUNT(status) OVER dw) AS FRACTION
+        FROM log
+        WINDOW dsw AS (PARTITION BY DATE_TRUNC('day', time), status),
+            dw AS (PARTITION BY DATE_TRUNC('day', time))
+        ) as error_counts
         WHERE status like '4%' AND fraction > 0.01
         ORDER BY fraction DESC;"""
 
-cursor.execute(sql_query1)
-problem1 = cursor.fetchall()
+cursor.execute(query3)
+query3_out_raw = cursor.fetchall()
+query3_out_tbl = tabulate(query3_out_raw, headers=['Date', 'Pct Error Response'],
+                          tablefmt="orgtbl")
 
-cursor.execute(sql_query2)
-problem2 = cursor.fetchall()
-
-cursor.execute(sql_query3)
-problem3 = cursor.fetchall()
 conn.close()
 
 print("\n")
-print("What are the three most popular articles of all time?")
-print(tabulate(problem1, headers=['Title', 'Views'], tablefmt="orgtbl"))
+print(question1)
+print("=" * len(question1))
+print(tabulate(query1_out_raw, headers=['Title', 'Views'], tablefmt="orgtbl"))
 print("\n")
-print("Who are the most popular article authors of all time?")
-print(tabulate(problem2, headers=['Author', 'Views'], tablefmt="orgtbl"))
+print(question2)
+print("=" * len(question2))
+print(tabulate(query2_out_raw, headers=['Author', 'Views'], tablefmt="orgtbl"))
 print("\n")
-print("On which days did more than 1% of requests lead to errors?")
-print(tabulate(problem3, headers=['Date', 'Pct Error Response'],
+print(question3)
+print("=" * len(question3))
+print(tabulate(query3_out_raw, headers=['Date', 'Pct Error Response'],
                tablefmt="orgtbl"))
 print("\n")
