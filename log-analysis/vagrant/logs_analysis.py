e231e0569017390415e033189854b6b86d6e30fb
Eric Hanley <ewhanley@gmail.com>
2017-09-12 23:56:28 -0700

    Moved query execution to a function

--- log-analysis/vagrant/logs_analysis.py
+++ log-analysis/vagrant/logs_analysis.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import psycopg2
 from tabulate import tabulate
 
-conn = psycopg2.connect(dbname='news')
-cursor = conn.cursor()
+DBNAME = 'news'
+
 
 question1 = "What are the three most popular articles of all time?"
+query1_header = ["Title", "Views"]
 query1 = """
         SELECT a.title, count(a.title) as views
         FROM articles a, log l
@@ -15,12 +16,8 @@ query1 = """
         GROUP BY a.title
         ORDER BY views DESC;"""
 
-cursor.execute(query1)
-query1_out_raw = cursor.fetchall()
-query1_out_tbl = tabulate(query1_out_raw, headers=['Title', 'Views'],
-                          tablefmt="orgtbl")
-
 question2 = "Who are the most popular article authors of all time?"
+query2_header = ["Author", "Views"]
 query2 = """
         SELECT au.name, COUNT(l.path) AS views
         FROM authors au, articles ar, log l
@@ -30,12 +27,8 @@ query2 = """
         GROUP BY au.name
         ORDER BY views DESC;"""
 
-cursor.execute(query2)
-query2_out_raw = cursor.fetchall()
-query2_out_tbl = tabulate(query2_out_raw, headers=['Author', 'Views'],
-                          tablefmt="orgtbl")
-
 question3 = "On which days did more than 1% of requests lead to errors?"
+query3_header = ["Date", "Pct Error"]
 query3 = """
         SELECT TO_CHAR(date, 'Month dd, YYYY') AS date,
             (ROUND(fraction * 100, 2)::text || '%') AS percent_errors
@@ -50,24 +43,33 @@ query3 = """
         WHERE status like '4%' AND fraction > 0.01
         ORDER BY fraction DESC;"""
 
-cursor.execute(query3)
-query3_out_raw = cursor.fetchall()
-query3_out_tbl = tabulate(query3_out_raw, headers=['Date', 'Pct Error Response'],
-                          tablefmt="orgtbl")
 
-conn.close()
+def get_query_results(db_name, query_string):
+    conn = psycopg2.connect(dbname=db_name)
+    cursor = conn.cursor()
+    cursor.execute(query_string)
+    query_result_raw = cursor.fetchall()
+    conn.close()
+    return query_result_raw
 
-print("\n")
-print(question1)
-print("=" * len(question1))
-print(tabulate(query1_out_raw, headers=['Title', 'Views'], tablefmt="orgtbl"))
-print("\n")
-print(question2)
-print("=" * len(question2))
-print(tabulate(query2_out_raw, headers=['Author', 'Views'], tablefmt="orgtbl"))
-print("\n")
-print(question3)
-print("=" * len(question3))
-print(tabulate(query3_out_raw, headers=['Date', 'Pct Error Response'],
-               tablefmt="orgtbl"))
-print("\n")
+
+def output_results_to_file(question, query_result, file_name):
+    output_string = "\n".join([question, query_result, "\n"])
+    with open(file_name, "w") as output_file:
+        output_file.write(output_string)
+
+
+query1_result_raw = get_query_results(DBNAME, query1)
+query2_result_raw = get_query_results(DBNAME, query2)
+query3_result_raw = get_query_results(DBNAME, query3)
+
+query1_result_tbl = tabulate(query1_result_raw, headers=query1_header,
+                             tablefmt="orgtbl")
+query2_result_tbl = tabulate(query2_result_raw, headers=query2_header,
+                             tablefmt="orgtbl")
+query3_result_tbl = tabulate(query3_result_raw, headers=query3_header,
+                             tablefmt="orgtbl")
+
+print(question1, query1_result_tbl, "\n", sep="\n")
+print(question2, query2_result_tbl, "\n", sep="\n")
+print(question3, query3_result_tbl, "\n", sep="\n")
