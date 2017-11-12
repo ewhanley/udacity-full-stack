e1b607fe900a1b6642ea7e2607a7dff986c661bf
Eric Hanley <ewhanley@gmail.com>
2017-11-12 10:56:19 -0800

    Adds index page and styling

--- backend-project/vagrant/project.py
+++ backend-project/vagrant/project.py
@@ -12,10 +12,43 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/<string:category>/JSON')
+@app.route('/cars/<string:category>/JSON')
 def categoryJSON(category):
     cars = session.query(Car).filter_by(category=category).all()
     return jsonify(Cars=[i.serialize for i in cars])
+
+
+@app.route('/cars/<string:category>/<int:car_id>/JSON')
+def carJSON(category, car_id):
+    car = session.query(Car).filter_by(category=category, id=car_id).one()
+    return jsonify(Car=car.serialize)
+
+
+@app.route('/')
+@app.route('/cars/')
+def showMainPage():
+    return render_template('index.html')
+
+
+@app.route('/cars/<string:category>/<int:car_id>')
+def readCar(category, car_id):
+    car = session.query(Car).filter_by(category=category, id=car_id).one()
+    return 'You can read information about a car here!'
+
+
+@app.route('/cars/create/', methods=['GET', 'POST'])
+def createCar():
+    return 'You can create a new car here!'
+
+
+@app.route('/cars/<string:category>/<int:car_id>/update/', methods=['GET', 'POST'])
+def updateCar(category, car_id):
+    return 'You can edit a car here!'
+
+
+@app.route('/cars/<string:category>/<int:car_id>/delete/', methods=['GET', 'POST'])
+def deleteCar(category, car_id):
+    return 'You can delete a car here!'
 
 
 if __name__ == '__main__':
