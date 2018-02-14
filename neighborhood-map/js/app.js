aee48e3252f50307ee822e9a29e811e3d3ec2930
Eric Hanley <ewhanley@gmail.com>
2018-02-13 22:55:55 -0800

    Adds location data and markers to map for each location

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -1,18 +1,46 @@
-var center = { lat: 45.676998, lng: -111.042934 };
-var zoom = 13;
 var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: center,
-    zoom: zoom
+    center: locations.map_center.location,
+    zoom: locations.map_center.zoom
+  });
+}
+
+function populateInfoWindow(marker, infowindow) {
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    infowindow.addListener('closeclick', function () {
+      infowindow.setMarker = null;
+    });
+  }
+}
+
+function initMarker(place) {
+  var self = this;
+  var largeInfowindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.location,
+    title: place.name,
+    animation: google.maps.Animation.DROP
+  });
+
+  marker.addListener('click', function () {
+    populateInfoWindow(this, largeInfowindow);
   });
 }
 
 
 var ViewModel = function () {
-  console.log("New viewmodel created!");
   var self = this;
+  locations.outfitters.forEach(initMarker);
 };
 
-ko.applyBindings(new ViewModel());
+var initApp = function () {
+  initMap();
+  ko.applyBindings(new ViewModel());
+};
---
+++ neighborhood-map/js/model.js
@@ -0,0 +1,54 @@
+var locations = {
+  map_center: {
+    name: "Bozeman, MT",
+    location: { lat: 45.676998, lng: -111.042934 },
+    zoom: 11
+  },
+  outfitters: [
+    {
+      name: "Montana Troutfitters",
+      location: { lat: 45.678831, lng: -111.060644 },
+      place_id: "ChIJE04D6bFFRVMRzGtWFJQsArY"
+    },
+    {
+      name: "Bozeman Angler",
+      location: { lat: 45.679581, lng: -111.036438 },
+      place_id: "ChIJTx3qbFpERVMRwDWuVsW4w7Y"
+    },
+    {
+      name: "The River's Edge Fly Shop",
+      location: { lat: 45.699347, lng: -111.045037 },
+      place_id: "ChIJ5yJRgzxERVMROW-fjpoU0nE"
+    },
+    {
+      name: "Montana Angler Fly Fishing",
+      location: { lat: 45.623592, lng: -111.072496 },
+      place_id: "ChIJ8agCEQFQRVMRGOdsnlvknN0"
+    },
+    {
+      name: "Fins & Feathers",
+      location: { lat: 45.670143, lng: -111.186106 },
+      place_id: "ChIJExVWlcZORVMR3jusBnqhWas"
+    },
+    /*     {
+          name: "Gallatin River Guides",
+          location: { lat: 45.25695, lng: -111.253816 },
+          place_id: "ChIJ2-B50QoQUFMR3YzlSZWKQhs"
+        }, */
+    {
+      name: "Montana Troutwranglers",
+      location: { lat: 45.781181, lng: -111.184296 },
+      place_id: "ChIJDdsuGR84RVMRwHQliR8HzB0"
+    },
+    {
+      name: "Montana's Last Best Outfitters",
+      location: { lat: 45.678295, lng: -111.191855 },
+      place_id: "ChIJMc1f5MtORVMRruDTrBTEtaM"
+    },
+    {
+      name: "Montana Angling Company",
+      location: { lat: 45.684291, lng: -111.027218 },
+      place_id: "ChIJ83FkWl1ERVMRohXKvIXbj9k"
+    }
+  ]
+};
