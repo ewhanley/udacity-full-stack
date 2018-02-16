75926e0625403639a27c3fd4729612605e1bd81e
Eric Hanley <ewhanley@gmail.com>
2018-02-15 23:23:28 -0800

    Adds location list to sidebar nav and toggles infowindows

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -18,13 +18,21 @@ var Outfitter = function (data) {
   });
   this.marker.addListener('click', function () {
     self.infoWindow.open(map, this);
-  })
+  });
 };
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: data.map_center.location,
     zoom: data.map_center.zoom
+  });
+
+  // This ensures that the map stays centered wherever the user last centered it.
+  // Solution found here: https://ao.gl/keep-google-map-v3-centered-when-browser-is-resized/
+  google.maps.event.addDomListener(window, 'resize', function () {
+    var currentCenter = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(currentCenter);
   });
 }
 
@@ -35,11 +43,20 @@ var ViewModel = function () {
   self.filteredList = ko.observableArray([]);
   self.filter = ko.observable();
 
+
   initMap();
 
   data.outfitters.forEach(function (outfitter) {
     self.initialList.push(new Outfitter(outfitter));
   });
+
+  self.selectedOutfitter = ko.observable(self.initialList()[0]);
+
+  self.toggleSelection = function () {
+    self.selectedOutfitter().infoWindow.setMap(null);
+    self.selectedOutfitter(this);
+    this.infoWindow.open(map, this.marker);
+  }
 
   self.w3_open = function w3_open() {
     document.getElementById("map").style.marginLeft = "25%";
